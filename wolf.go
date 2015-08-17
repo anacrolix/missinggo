9c62e0d0c80f9713eb30340d0b5ad6df2f18c15f
Matt Joiner <anacrolix@gmail.com>
2015-08-17 21:31:09 +1000

    wolf comments

--- wolf.go
+++ wolf.go
@@ -9,10 +9,14 @@ import (
 
 const debug = false
 
+// A Wolf represents some event that becomes less and less interesting as it
+// occurs. Call CryHeard to see if we should pay attention this time.
 type Wolf struct {
 	cries uint64
 }
 
+// Returns true less and less often. Convenient for exponentially decreasing
+// the amount of noise due to errors.
 func (me *Wolf) CryHeard() bool {
 	n := atomic.AddUint64(&me.cries, 1)
 	return n&(n-1) == 0
@@ -23,6 +27,8 @@ var (
 	wolves map[uintptr]*Wolf
 )
 
+// Calls CryHeard() on a Wolf that is unique to the callers program counter.
+// i.e. every CryHeard() expression has its own Wolf.
 func CryHeard() bool {
 	pc, file, line, ok := runtime.Caller(1)
 	if debug {
