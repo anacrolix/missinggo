be46d9a870d7769c2e4afd5f6cca87e5962e6c52
Matt Joiner <anacrolix@gmail.com>
2015-10-04 00:25:57 +1000

    httpfile.{Delete,OpenSectionReader}

--- httpfile/httpfile.go
+++ httpfile/httpfile.go
@@ -20,6 +20,30 @@ type File struct {
 	url    string
 }
 
+func OpenSectionReader(url string, off, n int64) (ret io.ReadCloser, err error) {
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return
+	}
+	req.Header.Set("Range", fmt.Sprintf("bytes=%d-%d", off, off+n-1))
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return
+	}
+	if resp.StatusCode == http.StatusNotFound {
+		err = ErrNotFound
+		resp.Body.Close()
+		return
+	}
+	if resp.StatusCode != http.StatusPartialContent {
+		err = fmt.Errorf("bad response status: %s", resp.Status)
+		resp.Body.Close()
+		return
+	}
+	ret = resp.Body
+	return
+}
+
 func Open(url string) *File {
 	return &File{
 		url: url,
@@ -175,4 +199,24 @@ func (me *File) Close() error {
 		me.r = nil
 	}
 	return nil
+}
+
+func Delete(urlStr string) (err error) {
+	req, err := http.NewRequest("DELETE", urlStr, nil)
+	if err != nil {
+		return
+	}
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return
+	}
+	resp.Body.Close()
+	if resp.StatusCode == http.StatusNotFound {
+		err = ErrNotFound
+		return
+	}
+	if resp.StatusCode != 200 {
+		err = fmt.Errorf("response: %s", resp.Status)
+	}
+	return
 }
