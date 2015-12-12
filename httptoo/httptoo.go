511dea2e19236648e2696870623f05eb46230209
Matt Joiner <anacrolix@gmail.com>
2015-12-12 14:04:30 +1100

    Expose path to NukeCookie

--- httptoo/httptoo.go
+++ httptoo/httptoo.go
@@ -20,12 +20,13 @@ func OriginatingProtocol(r *http.Request) string {
 }
 
 // Clears the named cookie for every domain that leads to the current one.
-func NukeCookie(w http.ResponseWriter, r *http.Request, name string) {
+func NukeCookie(w http.ResponseWriter, r *http.Request, name, path string) {
 	parts := strings.Split(missinggo.ParseHostPort(r.Host).Host, ".")
 	for i := range iter.N(len(parts) + 1) { // Include the empty domain.
 		http.SetCookie(w, &http.Cookie{
 			Name:   name,
 			MaxAge: -1,
+			Path:   path,
 			Domain: strings.Join(parts[i:], "."),
 		})
 	}
