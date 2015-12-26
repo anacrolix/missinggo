9bbe7d43affbc82fef075c4580c8782655e076ec
Matt Joiner <anacrolix@gmail.com>
2015-12-26 14:10:47 +1100

    httpfile: DefaultFS needs a non-nil http.Client

--- httpfile/defaultfs.go
+++ httpfile/defaultfs.go
@@ -1,6 +1,12 @@
 package httpfile
 
-var DefaultFS = &FS{}
+import (
+	"net/http"
+)
+
+var DefaultFS = &FS{
+	Client: http.DefaultClient,
+}
 
 // Returns the length of the resource in bytes.
 func GetLength(url string) (ret int64, err error) {
