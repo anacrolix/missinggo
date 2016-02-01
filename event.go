6953b1bb2c65915d46518b67c636a8b64c3cb66b
Matt Joiner <anacrolix@gmail.com>
2016-02-01 21:02:38 +1100

    Event.{IsSet,Wait}, and fix lazy init

--- event.go
+++ event.go
@@ -8,21 +8,42 @@ type Event struct {
 	closed bool
 }
 
-func (me *Event) C() <-chan struct{} {
-	me.mu.Lock()
-	defer me.mu.Unlock()
+func (me *Event) lazyInit() {
 	if me.ch == nil {
 		me.ch = make(chan struct{})
 	}
+}
+
+func (me *Event) C() <-chan struct{} {
+	me.mu.Lock()
+	defer me.mu.Unlock()
+	me.lazyInit()
 	return me.ch
 }
 
-func (me *Event) Set() {
+func (me *Event) Set() (first bool) {
 	me.mu.Lock()
 	defer me.mu.Unlock()
+	me.lazyInit()
 	if me.closed {
-		return
+		return false
 	}
 	close(me.ch)
 	me.closed = true
+	return true
+}
+
+func (me *Event) IsSet() bool {
+	me.mu.Lock()
+	defer me.mu.Unlock()
+	select {
+	case <-me.ch:
+		return true
+	default:
+		return false
+	}
+}
+
+func (me *Event) Wait() {
+	<-me.C()
 }
