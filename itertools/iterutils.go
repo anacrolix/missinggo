b85c8e2eadb2d460b625dc9b61731014b138ca20
Matt Joiner <anacrolix@gmail.com>
2016-02-07 21:53:34 +1100

    itertools: Add ForPerm()

--- itertools/iterutils.go
+++ itertools/iterutils.go
@@ -1,12 +1,42 @@
 package itertools
 
-// func ForIterable(i Iterable, cb func(value interface{}) (more bool)) (exhausted bool) {
-// 	it := i.Iter()
-// 	for it.Next() {
-// 		if !cb(it.Value()) {
-// 			it.Stop()
-// 			return false
-// 		}
-// 	}
-// 	return true
-// }
+import "math/rand"
+
+type seq struct {
+	i []int
+}
+
+// Creates sequence of values from [0, n)
+func newSeq(n int) seq {
+	return seq{make([]int, n, n)}
+}
+
+func (me seq) Index(i int) (ret int) {
+	ret = me.i[i]
+	if ret == 0 {
+		ret = i
+	}
+	return
+}
+
+func (me seq) Len() int {
+	return len(me.i)
+}
+
+// Remove the nth value from the sequence.
+func (me *seq) Delete(index int) {
+	me.i[index] = me.Index(me.Len() - 1)
+	me.i = me.i[:me.Len()-1]
+}
+
+func ForPerm(n int, callback func(i int) (more bool)) bool {
+	s := newSeq(n)
+	for s.Len() > 0 {
+		r := rand.Intn(s.Len())
+		if !callback(s.Index(r)) {
+			return false
+		}
+		s.Delete(r)
+	}
+	return true
+}
