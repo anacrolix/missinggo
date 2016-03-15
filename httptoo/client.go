fd58ea8b80ddf5da69b26d29bfcd9849f49fd8b2
Matt Joiner <anacrolix@gmail.com>
2016-03-15 16:45:05 +1100

    httptoo: Fix ClientTLSConfig()

--- httptoo/client.go
+++ httptoo/client.go
@@ -8,14 +8,12 @@ import (
 // Returns the http.Client's TLS Config, traversing and generating any
 // defaults along the way to get it.
 func ClientTLSConfig(cl *http.Client) *tls.Config {
-	rt := cl.Transport
-	if rt == nil {
-		rt = http.DefaultTransport
+	if cl.Transport == nil {
+		cl.Transport = http.DefaultTransport
 	}
-	tr := rt.(*http.Transport)
+	tr := cl.Transport.(*http.Transport)
 	if tr.TLSClientConfig == nil {
 		tr.TLSClientConfig = &tls.Config{}
 	}
-	ret := tr.TLSClientConfig
-	return ret
+	return tr.TLSClientConfig
 }
