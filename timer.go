7eed5b0293163e3ee5fe802e24f8cb23387474af
Matt Joiner <anacrolix@gmail.com>
2016-04-26 12:42:07 +1000

    Add StoppedFuncTimer

--- timer.go
+++ timer.go
@@ -1,3 +1,15 @@
 package missinggo
 
-// I would implement StopAndDrainTimer here, but time.Timer is broken in the standard library.
+import (
+	"math"
+	"time"
+)
+
+// Returns a time.Timer that calls f. The timer is initially stopped.
+func StoppedFuncTimer(f func()) (t *time.Timer) {
+	t = time.AfterFunc(math.MaxInt64, f)
+	if !t.Stop() {
+		panic("timer already fired")
+	}
+	return
+}
