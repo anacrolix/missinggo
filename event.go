c7e69781ec0ee4b10e621a6332c7d3461e9a19fe
Matt Joiner <anacrolix@gmail.com>
2016-05-07 18:18:55 +1000

    Don't have a mutex per Event, it's super slow

--- event.go
+++ event.go
@@ -2,45 +2,39 @@ package missinggo
 
 import "sync"
 
-// Events are threadsafe boolean flags that provide a channel that's closed
-// when its true.
+// Events are boolean flags that provide a channel that's closed when true.
 type Event struct {
-	mu     sync.Mutex
 	ch     chan struct{}
 	closed bool
 }
 
-func (me *Event) lazyInit() {
-	if me.ch == nil {
-		me.ch = make(chan struct{})
-	}
-}
-
-func (me *Event) C() <-chan struct{} {
-	me.mu.Lock()
-	me.lazyInit()
-	ch := me.ch
-	me.mu.Unlock()
+func (me *Event) LockedChan(lock sync.Locker) <-chan struct{} {
+	lock.Lock()
+	ch := me.C()
+	lock.Unlock()
 	return ch
 }
 
-func (me *Event) Clear() {
-	me.mu.Lock()
-	defer me.mu.Unlock()
-	me.lazyInit()
-	if !me.closed {
-		return
+func (me *Event) C() <-chan struct{} {
+	if me.ch == nil {
+		me.ch = make(chan struct{})
 	}
-	me.ch = make(chan struct{})
-	me.closed = false
+	return me.ch
+}
+
+func (me *Event) Clear() {
+	if me.closed {
+		me.ch = nil
+		me.closed = false
+	}
 }
 
 func (me *Event) Set() (first bool) {
-	me.mu.Lock()
-	defer me.mu.Unlock()
-	me.lazyInit()
 	if me.closed {
 		return false
+	}
+	if me.ch == nil {
+		me.ch = make(chan struct{})
 	}
 	close(me.ch)
 	me.closed = true
@@ -48,15 +42,7 @@ func (me *Event) Set() (first bool) {
 }
 
 func (me *Event) IsSet() bool {
-	me.mu.Lock()
-	ch := me.ch
-	me.mu.Unlock()
-	select {
-	case <-ch:
-		return true
-	default:
-		return false
-	}
+	return me.closed
 }
 
 func (me *Event) Wait() {
