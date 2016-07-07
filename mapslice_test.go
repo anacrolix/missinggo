797fc5dd606f07c7c8af5bed81b61c2a8dccefb0
Matt Joiner <anacrolix@gmail.com>
2016-07-07 20:25:00 +1000

    fix tests: missinggo.Sort->SortSlice

--- mapslice_test.go
+++ mapslice_test.go
@@ -9,7 +9,8 @@ import (
 func TestMapSlice(t *testing.T) {
 	sl := MapAsSlice(map[string]int{"two": 2, "one": 1})
 	assert.Len(t, sl, 2)
-	assert.EqualValues(t, []MapKeyValue{{"one", 1}, {"two", 2}}, Sort(sl, func(left, right MapKeyValue) bool {
+	SortSlice(sl, func(left, right MapKeyValue) bool {
 		return left.Key.(string) < right.Key.(string)
-	}))
+	})
+	assert.EqualValues(t, []MapKeyValue{{"one", 1}, {"two", 2}}, sl)
 }
