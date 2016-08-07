61ae8754b1c25f1450d3991077ebdd65b348dbcd
Matt Joiner <anacrolix@gmail.com>
2016-08-07 19:39:16 +1000

    cmd/http-file-server: Log what and where

--- cmd/http-file-server/main.go
+++ cmd/http-file-server/main.go
@@ -3,8 +3,15 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
-	log.Fatal(http.ListenAndServe(":8080", http.FileServer(http.Dir("."))))
+	const addr = ":8080"
+	dir, err := os.Getwd()
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("serving %q at %s", dir, addr)
+	log.Fatal(http.ListenAndServe(addr, http.FileServer(http.Dir(dir))))
 }
