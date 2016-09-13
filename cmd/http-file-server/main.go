86f106025d38e0baf732bb3e6761ab16e87d96cb
Matt Joiner <anacrolix@gmail.com>
2016-09-13 14:16:46 +1000

    Serve CORS headers

--- cmd/http-file-server/main.go
+++ cmd/http-file-server/main.go
@@ -2,11 +2,33 @@ package main
 
 import (
 	"log"
+	"net"
 	"net/http"
 	"os"
 
 	"github.com/anacrolix/tagflag"
 )
+
+func setIfGetHeader(w http.ResponseWriter, r *http.Request, set, get string) {
+	h := r.Header.Get(get)
+	if h == "" {
+		return
+	}
+	w.Header().Set(set, h)
+}
+
+func allowCORS(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if origin := r.Header.Get("Origin"); origin != "" {
+			w.Header().Set("Access-Control-Allow-Origin", origin)
+			w.Header().Set("Access-Control-Allow-Credentials", "true")
+			setIfGetHeader(w, r, "Access-Control-Allow-Methods", "Access-Control-Request-Method")
+			setIfGetHeader(w, r, "Access-Control-Allow-Headers", "Access-Control-Request-Headers")
+			w.Header().Set("Access-Control-Expose-Headers", "Content-Type")
+		}
+		h.ServeHTTP(w, r)
+	})
+}
 
 func main() {
 	var flags = struct {
