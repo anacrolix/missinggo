12075dbd1dc953d255dd3e7184fe6a3ec6d33bf5
Matt Joiner <anacrolix@gmail.com>
2017-01-05 00:51:50 +1100

    httptoo: Add RunHandler and RunHandlerFunc

--- httptoo/middleware.go
+++ httptoo/middleware.go
@@ -15,3 +15,11 @@ func WrapHandler(middleware []Middleware, h http.Handler) (ret http.Handler) {
 func WrapHandlerFunc(middleware []Middleware, hf func(http.ResponseWriter, *http.Request)) (ret http.Handler) {
 	return WrapHandler(middleware, http.HandlerFunc(hf))
 }
+
+func RunHandler(h http.Handler, w http.ResponseWriter, r *http.Request, middleware ...Middleware) {
+	WrapHandler(middleware, h).ServeHTTP(w, r)
+}
+
+func RunHandlerFunc(h func(http.ResponseWriter, *http.Request), w http.ResponseWriter, r *http.Request, middleware ...Middleware) {
+	WrapHandlerFunc(middleware, h).ServeHTTP(w, r)
+}
