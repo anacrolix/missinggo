9a698e8e48e621a0bb371620037f3226a3e17e89
Matt Joiner <anacrolix@gmail.com>
2017-06-11 20:06:43 +1000

    Make an error return part of the future interface

--- futures/future.go
+++ futures/future.go
@@ -2,13 +2,12 @@ package futures
 
 import "sync"
 
-func Start(fn func() interface{}) *F {
+func Start(fn func() (interface{}, error)) *F {
 	f := &F{
 		done: make(chan struct{}),
 	}
 	go func() {
-		result := fn()
-		f.setResult(result)
+		f.setResult(fn())
 	}()
 	return f
 }
@@ -16,23 +15,33 @@ func Start(fn func() interface{}) *F {
 type F struct {
 	mu     sync.Mutex
 	result interface{}
+	err    error
 	done   chan struct{}
 }
 
-func (f *F) Result() interface{} {
+func (f *F) Result() (interface{}, error) {
 	<-f.done
 	f.mu.Lock()
 	defer f.mu.Unlock()
-	return f.result
+	return f.result, f.err
+}
+
+func (f *F) MustResult() interface{} {
+	val, err := f.Result()
+	if err != nil {
+		panic(err)
+	}
+	return val
 }
 
 func (f *F) Done() <-chan struct{} {
 	return f.done
 }
 
-func (f *F) setResult(result interface{}) {
+func (f *F) setResult(result interface{}, err error) {
 	f.mu.Lock()
 	defer f.mu.Unlock()
 	f.result = result
+	f.err = err
 	close(f.done)
 }
