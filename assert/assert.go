2ecf71d1b7c0e98d17a9d7d3e7c5603fd4635839
Matt Joiner <anacrolix@gmail.com>
2017-12-14 10:43:05 +1100

    Add assert.{True,OneRowAffected}

--- assert/assert.go
+++ assert/assert.go
@@ -1,6 +1,7 @@
 package assert
 
 import (
+	"database/sql"
 	"reflect"
 
 	"fmt"
@@ -31,5 +32,19 @@ func Equal(x, y interface{}) {
 func StrictlyEqual(x, y interface{}) {
 	if x != y {
 		panic(fmt.Sprintf("%s != %s"))
+	}
+}
+
+func OneRowAffected(r sql.Result) {
+	count, err := r.RowsAffected()
+	Nil(err)
+	if count != 1 {
+		panic(count)
+	}
+}
+
+func True(b bool) {
+	if !b {
+		panic(b)
 	}
 }
