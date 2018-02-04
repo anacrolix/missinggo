173db517b5f8002d630f815e2097a1858c27f8ef
Matt Joiner <anacrolix@gmail.com>
2018-02-04 19:11:11 +1100

    reqctx: Some comments

--- reqctx/value.go
+++ reqctx/value.go
@@ -19,11 +19,13 @@ func (me contextValue) Get(ctx context.Context) interface{} {
 	return ctx.Value(me.key)
 }
 
+// Sets the value on the Request. It must not have been already set.
 func (me contextValue) SetRequestOnce(r *http.Request, val interface{}) *http.Request {
 	assert.Nil(me.Get(r.Context()))
 	return r.WithContext(context.WithValue(r.Context(), me.key, val))
 }
 
+// Returns a middleware that sets the value in the Request's Context.
 func (me contextValue) SetMiddleware(val interface{}) func(http.Handler) http.Handler {
 	return func(h http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
