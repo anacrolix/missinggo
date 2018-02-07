8c99c92dc2c0152eed1a987ad6c69f0c53fca880
Matt Joiner <anacrolix@gmail.com>
2018-02-07 13:46:40 +1100

    StatusResponseWriter support Hijack

--- httpresponsestatus.go
+++ httpresponsestatus.go
@@ -1,10 +1,12 @@
 package missinggo
 
+// todo move to httptoo as ResponseRecorder
+
 import (
+	"bufio"
+	"net"
 	"net/http"
 	"time"
-
-	"github.com/anacrolix/missinggo/assert"
 )
 
 // A http.ResponseWriter that tracks the status of the response. The status
@@ -14,23 +16,60 @@ type StatusResponseWriter struct {
 	Code         int
 	BytesWritten int64
 	Started      time.Time
-	Ttfb         time.Duration
+	Ttfb         time.Duration // Time to first byte
+	GotFirstByte bool
+	WroteHeader  Event
+	Hijacked     bool
 }
 
+var _ interface {
+	http.ResponseWriter
+	http.Hijacker
+} = (*StatusResponseWriter)(nil)
+
 func (me *StatusResponseWriter) Write(b []byte) (n int, err error) {
-	if me.BytesWritten == 0 && len(b) > 0 {
-		assert.False(me.Started.IsZero())
+	// Exactly how it's done in the standard library. This ensures Code is
+	// correct.
+	if !me.WroteHeader.IsSet() {
+		me.WriteHeader(http.StatusOK)
+	}
+	if !me.GotFirstByte && len(b) > 0 {
+		if me.Started.IsZero() {
+			panic("Started was not initialized")
+		}
 		me.Ttfb = time.Since(me.Started)
+		me.GotFirstByte = true
 	}
 	n, err = me.ResponseWriter.Write(b)
 	me.BytesWritten += int64(n)
-	if me.Code == 0 {
-		me.Code = 200
-	}
 	return
 }
 
 func (me *StatusResponseWriter) WriteHeader(code int) {
 	me.ResponseWriter.WriteHeader(code)
-	me.Code = code
+	if !me.WroteHeader.IsSet() {
+		me.Code = code
+		me.WroteHeader.Set()
+	}
+}
+
+func (me *StatusResponseWriter) Hijack() (c net.Conn, b *bufio.ReadWriter, err error) {
+	me.Hijacked = true
+	c, b, err = me.ResponseWriter.(http.Hijacker).Hijack()
+	if b.Writer.Buffered() != 0 {
+		panic("unexpected buffered writes")
+	}
+	c = responseConn{c, me}
+	return
+}
+
+type responseConn struct {
+	net.Conn
+	s *StatusResponseWriter
+}
+
+func (me responseConn) Write(b []byte) (n int, err error) {
+	n, err = me.Conn.Write(b)
+	me.s.BytesWritten += int64(n)
+	return
 }
