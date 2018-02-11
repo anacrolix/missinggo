3fcd3428299ff464ed1bb8f9e14956f8e47dd880
Matt Joiner <anacrolix@gmail.com>
2018-02-11 15:16:33 +1100

    slices.FilterInPlace: Take a typed function and a pointer to the value

--- slices/filter.go
+++ slices/filter.go
@@ -2,12 +2,12 @@ package slices
 
 import "reflect"
 
-func FilterInPlace(sl interface{}, f func(interface{}) bool) {
+func FilterInPlace(sl interface{}, f interface{}) {
 	v := reflect.ValueOf(sl).Elem()
 	j := 0
 	for i := 0; i < v.Len(); i++ {
 		e := v.Index(i)
-		if f(e.Interface()) {
+		if reflect.ValueOf(f).Call([]reflect.Value{e.Addr()})[0].Bool() {
 			v.Index(j).Set(e)
 			j++
 		}
