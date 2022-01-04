f94266d1aa0c199bbf864afa8bf687f14438a4f2
Jarjar Binks <>
2022-01-03 20:42:41 -0500

    Added check to ignore unexported struct fields in IsEmptyValue

--- empty_value_test.go
+++ empty_value_test.go
@@ -11,3 +11,21 @@ func TestEmptyValue(t *testing.T) {
 	assert.True(t, IsEmptyValue(reflect.ValueOf(false)))
 	assert.False(t, IsEmptyValue(reflect.ValueOf(true)))
 }
+
+func TestUnexportedField(t *testing.T) {
+	type FooType1 struct {
+		Bar  int
+		Dog  bool
+		fish string
+	}
+	fooInstance := FooType1{}
+
+	assert.True(t, IsEmptyValue(reflect.ValueOf(fooInstance)))
+
+	fooInstance2 := FooType1{fish: "fishy"}
+	assert.True(t, IsEmptyValue(reflect.ValueOf(fooInstance2)))
+
+	fooInstance3 := FooType1{Bar: 5}
+
+	assert.False(t, IsEmptyValue(reflect.ValueOf(fooInstance3)))
+}
