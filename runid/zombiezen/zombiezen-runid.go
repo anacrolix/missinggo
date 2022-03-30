fbac3e5cd9c0bb304de67be8c455994210c84a47
Matt Joiner <anacrolix@gmail.com>
2022-03-30 11:25:24 +1100

    Add runid/zombiezen

---
+++ runid/zombiezen/zombiezen-runid.go
@@ -0,0 +1,18 @@
+package zombiezen_runid
+
+import (
+	"zombiezen.com/go/sqlite"
+	"zombiezen.com/go/sqlite/sqlitex"
+	"github.com/anacrolix/missinggo/expect"
+	"github.com/anacrolix/missinggo/v2/runid"
+)
+
+func New(db *sqlite.Conn) *runid.T {
+	err := sqlitex.ExecScript(db, `
+CREATE TABLE if not exists runs (started datetime default (datetime('now')));
+insert into runs default values;
+`)
+	expect.Nil(err)
+	ret := runid.T(db.LastInsertRowID())
+	return &ret
+}
