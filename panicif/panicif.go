15fa01bc51e5ab0e80432ebb40bac1a64cbbc138
Matt Joiner <anacrolix@gmail.com>
2024-08-21 00:40:47 +1000

    Add some panicif functions

--- panicif/panicif.go
+++ panicif/panicif.go
@@ -1,7 +1,35 @@
 package panicif
 
-func NotNil(x interface{}) {
+import (
+	"fmt"
+)
+
+func NotNil(x any) {
 	if x != nil {
+		panic(x)
+	}
+}
+
+func Err(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func NotEq(a, b any) {
+	if a != b {
+		panic(fmt.Sprintf("%v != %v", a, b))
+	}
+}
+
+func True(x bool) {
+	if x {
+		panic(x)
+	}
+}
+
+func False(x bool) {
+	if !x {
 		panic(x)
 	}
 }
