ddd9eb19879746f2495c93ad4c93ef37b7517db3
Matt Joiner <anacrolix@gmail.com>
2025-06-10 12:55:50 +1000

    Forward deprecated prometheus.NewExpvarCollector

---
+++ prometheus/deprecated.go
@@ -0,0 +1,8 @@
+package xprometheus
+
+import (
+	expvar_prometheus "github.com/anacrolix/missinggo/v2/expvar-prometheus"
+)
+
+// Deprecated: Use expvar_prometheus package instead.
+var NewExpvarCollector = expvar_prometheus.NewExpvarCollector
